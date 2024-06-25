75799fac5f0f6c6b70cd1f9b32d58e9b9c21b71a
abhisek <abhisek.datta@gmail.com>
2024-06-25 09:41:30 +0530

    ci: Fix repo format for js2py

--- ecosystems/python/js2py/build.sh
+++ ecosystems/python/js2py/build.sh
@@ -1,1 +1,3 @@
-PKG_GIT_URL="https://github.com/PiotrDabkowski/Js2Py"
+# Restricting to GitHub owner/repo format since we are doing the POC
+# using GitHub Actions only
+PKG_GIT_URL="PiotrDabkowski/Js2Py"
