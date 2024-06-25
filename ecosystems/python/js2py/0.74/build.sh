32b30748404b630f5f64dc2f7c12e1864eaa4695
abhisek <abhisek.datta@gmail.com>
2024-06-25 10:10:28 +0530

    ci: Update python build workflow to include dist gen

--- ecosystems/python/js2py/0.74/build.sh
+++ ecosystems/python/js2py/0.74/build.sh
@@ -3,3 +3,15 @@
 # in setup.py
 
 PKG_GIT_COMMIT_SHA="2e017b86e2f18a6c8a842293b1687f2ce7baa12e"
+
+# The caller of these functions must set the $PWD
+# to directory of the package source code
+
+function build_package {
+  pip install -r ./requirements.txt
+  python setup.py sdist
+}
+
+function get_artifact_path {
+  echo -n "dist/Js2Py-0.74.tar.gz"
+}
